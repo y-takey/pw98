ae7a2c9a9bf578fe5bee6e011c9bb20829580e68
takeyuichi <y.takey@gmail.com>
2018-04-30 16:02:16 +0900

    Implement to select and move window

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import React, { Component } from "react";
 import Window from "./Window";
 import parse from "./configParser";
 
+const keyDesc = _.map(
+  {
+    "<-/->": "Move",
+    "0": "Deselect",
+    "1-9": "Select",
+    c: "Clear",
+    m: "Maximize/Minimize",
+    r: "Restart"
+  },
+  (v, k) => ` ${k}: ${v}`
+).join(", ");
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -13,18 +25,93 @@ class App extends Component {
     this.state = {
       height,
       width,
+      selectedNo: 0,
       procs: parse(props.config, { top: 0, left: 0, height, width })
     };
   }
 
+  moveNo = (amount, _key) => {
+    let selectedNo = this.state.selectedNo + amount;
+    const maxNo = this.state.procs.length;
+    if (selectedNo < 0) {
+      selectedNo = maxNo;
+    } else if (selectedNo > maxNo) {
+      selectedNo = 0;
+    }
+
+    this.setState({ selectedNo });
+  };
+
+  selectNo = key => {
+    let selectedNo = Number(key.ch);
+    const maxNo = this.state.procs.length;
+    if (selectedNo > maxNo) selectedNo = maxNo;
+
+    this.setState({ selectedNo });
+  };
+
+  clearLog = () => {};
+
+  toggleWindow = () => {};
+
+  restartProc = () => {};
+
+  keyBindings = keyName => {
+    if (!this._keyMap) {
+      this._keyMap = {
+        left: _.partial(this.moveNo, -1),
+        right: _.partial(this.moveNo, 1),
+        "0": this.selectNo,
+        "1": this.selectNo,
+        "2": this.selectNo,
+        "3": this.selectNo,
+        "4": this.selectNo,
+        "5": this.selectNo,
+        "6": this.selectNo,
+        "7": this.selectNo,
+        "8": this.selectNo,
+        "9": this.selectNo,
+        c: this.clearLog,
+        m: this.toggleWindow,
+        r: this.restartProc
+      };
+    }
+    return this._keyMap[keyName];
+  };
+
+  handleKeypress = (_char, key) => {
+    const func = this.keyBindings(key.name || key.ch);
+    if (func) func(key);
+  };
+
   render() {
-    const { height, width, procs } = this.state;
+    const { height, width, procs, selectedNo } = this.state;
+    const containerOptions = {
+      keys: true,
+      keyable: true,
+      mouse: false,
+      focused: true,
+      scrollable: false
+    };
 
     return (
-      <box top={0} left={0} height={height} width={width}>
+      <box
+        {...containerOptions}
+        top={0}
+        left={0}
+        height={height}
+        width={width}
+        onKeypress={this.handleKeypress}
+      >
         {procs.map((props, i) => (
-          <Window {...props} index={i + 1} key={`proc-${i}`} />
+          <Window
+            {...props}
+            index={i + 1}
+            key={`proc-${i}`}
+            selected={selectedNo - 1 === i}
+          />
         ))}
+        <text top={height}>{keyDesc}</text>
       </box>
     );
   }
--- src/Window.js
+++ src/Window.js
@@ -1,18 +1,29 @@
 import React, { Component } from "react";
 
 class Window extends Component {
+  componentDidMount() {
+    this.timer = setInterval(() => {
+      this.refs.log.add(new Date());
+    }, 2000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
   render() {
-    const { top, left, height, width, index, name } = this.props;
+    const { top, left, height, width, index, name, selected } = this.props;
+
     const options = {
       top,
       left,
       height,
       width,
       border: { type: "line" },
-      style: { border: { fg: "white" } }
+      style: { border: { fg: selected ? "cyan" : "white" } }
     };
 
-    return <log ref="log" {...options} label={` ${index}: ${name} `} />;
+    return <log ref="log" {...options} label={` [${index}] ${name} `} />;
   }
 }
 
